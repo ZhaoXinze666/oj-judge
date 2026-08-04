7eb19f907944faf0209541de975a45810cac7f19
ZhaoXinze666 <327384820@qq.com>
2026-08-04 08:48:40 +0800

    Submit #9 for problem 2

---
+++ submissions/9.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int a,b;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin>>a>>b;
+    cout<<a+b;
+    return 0;
+}
