b63c132f33ffe81ee481332f413cf58beba342ae
ZhaoXinze666 <327384820@qq.com>
2026-08-05 09:23:10 +0800

    Submit #17 for problem 2

---
+++ submissions/17.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    while (1){
+      cout<<"udihuhsfdjkhhfdiushihdfsiuheuihsiu";
+       cout<<"udihuhsfdjkhhfdiushihdfsiuheuihsiu";
+       cout<<"udihuhsfdjkhhfdiushihdfsiuheuihsiu";
+       cout<<"udihuhsfdjkhhfdiushihdfsiuheuihsiu";
+       cout<<"udihuhsfdjkhhfdiushihdfsiuheuihsiu";
+       cout<<"udihuhsfdjkhhfdiushihdfsiuheuihsiu";
+       cout<<"udihuhsfdjkhhfdiushihdfsiuheuihsiu";
+       cout<<"udihuhsfdjkhhfdiushihdfsiuheuihsiu";
+       cout<<"udihuhsfdjkhhfdiushihdfsiuheuihsiu";
+       cout<<"udihuhsfdjkhhfdiushihdfsiuheuihsiu";
+       cout<<"udihuhsfdjkhhfdiushihdfsiuheuihsiu";
+       cout<<"udihuhsfdjkhhfdiushihdfsiuheuihsiu";
+      cout<<"udihuhsfdjkhhfdiushihdfsiuheuihsiu";
+       cout<<"udihuhsfdjkhhfdiushihdfsiuheuihsiu";
+       cout<<"udihuhsfdjkhhfdiushihdfsiuheuihsiu";
+       cout<<"udihuhsfdjkhhfdiushihdfsiuheuihsiu";
+       cout<<"udihuhsfdjkhhfdiushihdfsiuheuihsiu";
+       cout<<"udihuhsfdjkhhfdiushihdfsiuheuihsiu";
+       cout<<"udihuhsfdjkhhfdiushihdfsiuheuihsiu";
+       cout<<"udihuhsfdjkhhfdiushihdfsiuheuihsiu";
+       cout<<"udihuhsfdjkhhfdiushihdfsiuheuihsiu";
+       cout<<"udihuhsfdjkhhfdiushihdfsiuheuihsiu";
+       cout<<"udihuhsfdjkhhfdiushihdfsiuheuihsiu";
+       cout<<"udihuhsfdjkhhfdiushihdfsiuheuihsiu";
+      
+    }
+    return 0;
+}
