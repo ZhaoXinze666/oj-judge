9a9ee9d96ceaba1ffdd4a33994915988c4c53de9
ZhaoXinze666 <327384820@qq.com>
2026-08-11 10:50:20 +0800

    Submit #21 for problem 2

---
+++ submissions/21.py
@@ -0,0 +1,9 @@
+# Write your solution here
+def solve():
+  a, b = map(int, input().split())
+  print(a + b)
+
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.read()
+    print(solve())
