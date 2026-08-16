ed0067de17a2d250f8b2044b00e846adbff080ee
ZhaoXinze666 <327384820@qq.com>
2026-08-16 15:10:02 +0800

    Submit #23 for problem 2

---
+++ submissions/23.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int a;
+    int b;
+    cin>>a>>b;
+  cout<<a+b;
+    return 0;
+}
